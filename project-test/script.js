771f50f06918bfaef6b3de4164bf2ac5760376a8
Jon King <usmcnomad@gmail.com>
2020-05-26 12:24:24 -0700

    adjusted js, styles, and html to add input form

--- project-test/script.js
+++ project-test/script.js
@@ -1,15 +1,24 @@
+
+//click the three diagonal lines to expand the matrix view//
 function expandDiv() {
     document.getElementById('input-form').style.width = '0%';
+    document.getElementById('input-form').style.opacity = '0';
     document.getElementById('container-template').style.width = '100%';
     document.getElementById('container-template').style.opacity = '1';
     document.getElementById('writeup-1-open').style.opacity = '0';
+    document.getElementById('writeup-1-open').style.width = '0px';
     document.getElementById('writeup-1-close').style.opacity = '1';
+    document.getElementById('writeup-1-close').style.width = '20px';
 }
 
+//click the x to close the matrix view and return to the input form//
 function closeDiv() {
     document.getElementById('input-form').style.width = '100%';
+    document.getElementById('input-form').style.opacity = '1';
     document.getElementById('container-template').style.width = '0%';
     document.getElementById('container-template').style.opacity = '0';
     document.getElementById('writeup-1-open').style.opacity = '1';
+    document.getElementById('writeup-1-open').style.width = '20px';
     document.getElementById('writeup-1-close').style.opacity = '0';
+    document.getElementById('writeup-1-close').style.width = '0px';
 }
